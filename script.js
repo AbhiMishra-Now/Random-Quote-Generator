9c8373d6f08ee98e450dc1e9f24e8a87160488f2
AbhiMishra-Now <abhimishrasultanpur4@gmail.com>
2022-10-13 21:12:42 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+ // Random Inspirational Quote Generator
+const quotes = {
+    beginning: [
+        'You are ', 
+        'You strength is ',
+        'Your life is ',
+        'Your energy is ',
+        'You look '
+    ],
+    
+    ending: [
+        ' like a god/goddess!',
+        ' radiating!',
+        ' magnificent!',
+        ' out of this world!',
+        ' wonderful!',
+        ' contagiously positive!',
+        ' extraordinary!'
+         
+    ]
+};
+
+const randomQuote = () => {
+    let firstHalf = quotes.beginning[Math.floor(Math.random() * quotes.beginning.length)];
+    let secondHalf = quotes.ending[Math.floor(Math.random() * quotes.beginning.length)];
+    return firstHalf + secondHalf;
+}
+
+const container = document.querySelector('.container');
+const outputBox = document.querySelector('.output');
+const button = document.querySelector('.button');
+
+button.onclick = function() {
+    outputBox.style.cssText = 'animation: outputBox 2s ease-in; \
+                                font-size: 3rem; \
+                                font-weight: 400'; 
+    outputBox.innerHTML = randomQuote();
+    outputBox.onanimationend = function() {
+        outputBox.style.cssText = 'font-size: 3rem; \
+                                    font-weight: 400';
+    }
+};
